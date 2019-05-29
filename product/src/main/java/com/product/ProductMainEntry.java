f0f3cfcca35ce916c6ef852cd7e016066fee0980
gokulashsagar@gmail.com <gokulashsagar@gmail.com>
2019-05-29 13:02:07 +0530

    yml changes

--- product/src/main/java/com/product/ProductMainEntry.java
+++ product/src/main/java/com/product/ProductMainEntry.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ProductMainEntry {
 	
 	public static void main(String[] args) {
